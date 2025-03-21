ef71b685d2f3af8c564a2133f659f0889066dbcd
kosmoszzang <135092138+kosmoszzang@users.noreply.github.com>
2025-03-21 13:36:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-gsap.registerPlugin(ScrollTrigger)
+// gsap.registerPlugin(ScrollTrigger);
 function sideMnu(){
         $('.sideMnu').toggleClass('close');
         $('.sideConts').toggleClass('active');
